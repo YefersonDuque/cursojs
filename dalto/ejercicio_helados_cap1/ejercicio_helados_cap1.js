3788754c64bee93089f822f59e54f50a0550e6c7
YefersonDuque <duqueyeferson@gmail.com>
2025-05-15 07:57:57 -0500

    primer parte del ejercicio capitulo 1

---
+++ dalto/ejercicio_helados_cap1/ejercicio_helados_cap1.js
@@ -0,0 +1,109 @@
+dineroCofla = prompt('¿Cuánto dinero tienes cofla?');
+dineroRoberto = prompt('¿Cuánto dinero tienes Roberto?');
+dineroPedro = prompt('¿Cuánto dinero tienes Pedro?');
+dineroCofla = parseInt(dineroCofla);
+
+if (dineroCofla >= 0.6 && dineroCofla < 1) {
+    alert("Cofla, comprate el helado de agua");
+    alert(" y te sobra " + (dineroCofla - 0.6) );
+}
+else if (dineroCofla >= 1 && dineroCofla < 1.6) {
+    alert("Cofla, comprate el helado de crema");
+    alert(" y te sobra " + (dineroCofla - 1));
+
+}
+else if (dineroCofla >= 1.6 && dineroCofla < 1.7) {
+    alert("Cofla, comprate el helado de heladix");
+    alert(" y te sobra " + (dineroCofla - 1.6));
+
+}
+else if (dineroCofla >= 1.7 && dineroCofla < 1.8) {
+    alert("Cofla, comprate el helado de heladovich");
+    alert(" y te sobra " + (dineroCofla - 1.7));
+
+}
+else if (dineroCofla >= 1.8 && dineroCofla < 2.9) {
+    alert("Cofla, Comprate el helado de helardo");
+    alert(" y te sobra " + (dineroCofla - 1.8));
+
+}
+else if (dineroCofla >= 2.9) {
+    alert("Cofla, comprate el helado con confites o el postre de 1/4kg");
+    alert(" y te sobra " + (dineroCofla - 2.9));
+
+}
+else {
+    alert("Cofla, no te alcanza")
+}
+
+if (dineroRoberto >= 0.6 && dineroRoberto < 1) {
+    alert("Roberto, comprate el helado de agua");
+    alert(" y te sobra " + (dineroRoberto - 0.6));
+
+}
+else if (dineroRoberto >= 1 && dineroRoberto < 1.6) {
+    alert("Roberto, comprate el helado de crema");
+    alert(" y te sobra " + (dineroRoberto - 1));
+
+}
+else if (dineroRoberto >= 1.6 && dineroRoberto < 1.7) {
+    alert("Roberto, comprate el helado de heladix");
+    alert(" y te sobra " + (dineroRoberto - 1.6));
+
+}
+else if (dineroRoberto >= 1.7 && dineroRoberto < 1.8) {
+    alert("Roberto, comprate el helado de heladovich");
+    alert(" y te sobra " + (dineroRoberto - 1.7));
+
+}
+else if (dineroRoberto >= 1.8 && dineroRoberto < 2.9) {
+    alert("Roberto, comprate el helado de helardo");
+    alert(" y te sobra " + (dineroRoberto - 1.8));
+
+}
+else if (dineroRoberto >= 2.9) {
+    alert("Roberto, comprate el helado con confites o el postre de 1/4kg");
+    alert(" y te sobra " + (dineroRoberto - 2.9));
+
+}
+else {
+    alert("Roberto, no te alcanza")
+}
+if (dineroPedro >= 0.6 && dineroPedro < 1) {
+    alert("Pedro, comprate el helado de agua");
+    alert(" y te sobra " + (dineroPedro - 0.6));
+
+    
+}
+else if (dineroPedro >= 1 && dineroPedro < 1.6) {
+    alert("Pedro, comprate el helado de crema");
+    alert(" y te sobra " + (dineroPedro - 1));
+
+    
+}
+else if (dineroPedro >= 1.6 && dineroPedro < 1.7) {
+    alert("Pedro, comprate el helado de heladix");
+    alert(" y te sobra " + (dineroPedro - 1.6));
+
+    
+}
+else if (dineroPedro >= 1.7 && dineroPedro < 1.8) {
+    alert("Pedro, comprate el helado de heladovich");
+    alert(" y te sobra " + (dineroPedro - 1.7));
+
+}
+else if (dineroPedro >= 1.8 && dineroPedro < 2.9) {
+    alert("Pedro, comprate el helado de helardo");
+    alert(" y te sobra " + (dineroPedro - 1.8));
+
+    
+}
+else if (dineroPedro >= 2.9) {
+    alert("Pedro, comprate el helado con confites o el postre de 1/4kg");
+    alert(" y te sobra " + (dineroPedro - 2.9));
+
+    
+}
+else {
+    alert("Pedro, no te alcanza")
+}
