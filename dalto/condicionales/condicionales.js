457f487a50c29e0cc5227514c6e44de95218d17d
YefersonDuque <duqueyeferson@gmail.com>
2025-05-14 23:12:50 -0500

    condicional if

---
+++ dalto/condicionales/condicionales.js
@@ -0,0 +1,7 @@
+//-----------IF-------------//
+//Si es solo una condicion no hay que poner '{}', si tiene más de pone else if
+if(true)
+    document.writeln("Es true<br>");
+else 
+    document.writeln("Es falso<br>");
+
