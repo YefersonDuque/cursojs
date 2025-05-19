1e96fa2f8ef3df2d1f8b73f3ab27cff4a0c783d3
YefersonDuque <duqueyeferson@gmail.com>
2025-05-19 17:30:56 -0500

    primer parte de console

---
+++ dalto/console/console.js
@@ -0,0 +1,19 @@
+//FUNCIONES DE REGISTRO
+console.clear(); //Para limpiar la consola
+console.assert(5<4); //Si el resultado es bueno, no hace nada, su es error lo muestra por consola. ERROR
+console.assert(5>4); //Si el resultado es bueno, no hace nada, su es error lo muestra por consola. BIEN
+console.error("menssaje de error por consola");//No smuestra un mensaje si hay error en el código por emdio de consola indicando donde esta revisando.
+console.info("Mensaje informativo por consola"); //Mensaje de error por consola
+console.table([2,3,4,5,6,4,3,5]) //Nos muestra en una tabla con los elementos del arreglo
+console.warn("OK") //No nos tira error pero nos tira una advertencia de que falta algo por hacer como un boton por presionar o algo así...
+console.dir([1,4,3,2,3])//Nos muestra el tipo de elemento si es un array... y su contenido
+
+//FUNCIONES DE CONTEO
+console.count() // Hace un conteo de las veces que se ejecuto
+console.count() // 
+console.count() // 
+console.countReset() // Esto lo resetea para iniciar de nuevo el conteo
+console.count() // 
+
+//FUNCIONES DE AGRUPACIÓN
+
