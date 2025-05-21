1d56975225bfc94c00cea1d0c98a594ceb1db58f
YefersonDuque <duqueyeferson@gmail.com>
2025-05-20 22:31:08 -0500

    corrección de errores al crear react - primeros pasos

---
+++ react/02-intro-javascript/src/bases/01-const-left.js
@@ -0,0 +1,14 @@
+
+
+const nombre = "Yeferson";
+const apellido = "Duque";
+let dado= 4;
+dado=5;
+console.log(`${nombre}${apellido}`);
+// eslint-disable-next-line no-constant-condition
+if(true){
+    const dado = 6;
+    console.log(dado);
+    
+}
+console.log(dado);
---
+++ react/02-intro-javascript/src/index.js
@@ -0,0 +1,7 @@
+
+const nombre = "Yefer";
+const apellido = "Apellido";
+
+const nombreCompleto = `${nombre} ${apellido}`;
+
+console.log(nombreCompleto);
