cb0865c3bc938653c51f92aaac6573367260bdfb
YefersonDuque <duqueyeferson@gmail.com>
2025-05-23 16:00:01 -0500

    fin sesion 6 - inicio sesion 9

--- react/04-git-expert-app/src/main.jsx
+++ react/04-git-expert-app/src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { GifExpertApp } from './GifExpertApp';
+import './styles.css'
 
 createRoot(document.getElementById('root')).render(
+  // El modo estricto solo se aplica en desarrollo
   <StrictMode>
-    <App />
+    <GifExpertApp />
   </StrictMode>,
 )
